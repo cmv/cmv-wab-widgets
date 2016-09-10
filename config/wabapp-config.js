ea3eb6e06996ef3630a419e700626f2d29832407
Tim McGee <github@mmconsult.com>
2016-09-10 00:36:03 -0700

    tap into the map's setInfoWindowOnClick method

--- config/wabapp-config.js
+++ config/wabapp-config.js
@@ -1,13 +1,24 @@
 define({
+    'wabVersion': '2.1',
     'theme': {
         'name': 'cmv'
+    },
+    'isRTL': false,
+    'httpProxy': {
+        'useProxy': false,
+        'alwaysUseProxy': false,
+        'url': '',
+        'rules': [{
+            'urlPrefix': '',
+            'proxyUrl': ''
+        }]
     },
     'geometryService': 'https://utility.arcgisonline.com/arcgis/rest/services/Geometry/GeometryServer',
     'map': {
         'id': 'map',
         '2D': true,
         '3D': false,
-        'itemId': '8bf7167d20924cbf8e25e7b11c7c502c',
+        'itemId': '8bf7167d20924cbf8e25e7b11c7c502c', // ESRI Streets Basemap
         'portalUrl': 'https://www.arcgis.com/'
     }
 });
